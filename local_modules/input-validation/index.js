a26e166d665b4e648a0e550417e6b80b48a4bf92
jejdacz <markhunter@seznam.cz>
2018-08-31 18:10:16 +0200

    input validation build

--- local_modules/input-validation/index.js
+++ local_modules/input-validation/index.js
@@ -3,14 +3,12 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-var isValidName = (exports.isValidName = function isValidName(w) {
-  return /^[A-Za-z-' ]+$/.test(w.trim());
-});
+var isValidName = exports.isValidName = function isValidName(w) {
+  return (/^[A-Za-z-'. ]+$/.test(w.trim())
+  );
+};
 
-var getLastDayOfMonth = (exports.getLastDayOfMonth = function getLastDayOfMonth(
-  year,
-  month
-) {
+var getLastDayOfMonth = exports.getLastDayOfMonth = function getLastDayOfMonth(year, month) {
   if (month === undefined || year === undefined) {
     throw new Error("undefined argument(s)");
   }
@@ -22,23 +20,13 @@ var getLastDayOfMonth = (exports.getLastDayOfMonth = function getLastDayOfMonth(
     throw new Error("bad arguments");
   }
 
-  var isLeapYear = year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0);
+  var isLeapYear = year % 400 === 0 || year % 4 === 0 && year % 100 !== 0;
   var longMonths = [1, 3, 5, 7, 8, 10, 12];
 
-  return longMonths.includes(month)
-    ? 31
-    : month !== 2
-      ? 30
-      : isLeapYear
-        ? 29
-        : 28;
-});
+  return longMonths.includes(month) ? 31 : month !== 2 ? 30 : isLeapYear ? 29 : 28;
+};
 
-var isValidDate = (exports.isValidDate = function isValidDate(
-  year,
-  month,
-  day
-) {
+var isValidDate = exports.isValidDate = function isValidDate(year, month, day) {
   if (day === undefined || month === undefined || year === undefined) {
     throw new Error("undefined argument(s)");
   }
@@ -47,23 +35,10 @@ var isValidDate = (exports.isValidDate = function isValidDate(
   month = parseInt(month);
   year = parseInt(year);
 
-  if (
-    day == NaN ||
-    month == NaN ||
-    year == NaN ||
-    year < 0 ||
-    year > 9999 ||
-    month < 1 ||
-    month > 12 ||
-    day < 1 ||
-    day > getLastDayOfMonth(year, month)
-  ) {
+  if (day == NaN || month == NaN || year == NaN || year < 0 || year > 9999 || month < 1 || month > 12 || day < 1 || day > getLastDayOfMonth(year, month)) {
     return false;
   }
   return true;
-});
-
-exports.default = {
-  isValidName: isValidName,
-  isValidDate: isValidDate
 };
+
+exports.default = { isValidName: isValidName, isValidDate: isValidDate };
