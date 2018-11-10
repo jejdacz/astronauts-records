0cbd8b7e3862189d37e4c3797892f453e02dcfbe
jejdacz <markhunter@seznam.cz>
2018-11-10 23:59:53 +0100

    table controls css

--- client/src/components/ConfirmButton/ConfirmButton.js
+++ client/src/components/ConfirmButton/ConfirmButton.js
@@ -3,7 +3,6 @@ import PropTypes from "prop-types";
 import LinkButton from "../LinkButton/LinkButton";
 import { joinToStringBySpace as jstr } from "../../utils/joinToString.js";
 import { withStateHandlers } from "recompose";
-import styles from "./ConfirmButton.module.css";
 
 const openHandler = withStateHandlers(
   () => ({
@@ -18,29 +17,6 @@ const openHandler = withStateHandlers(
     })
   }
 );
-/*
-const ConfirmButtonBase = ({
-  isOpen,
-  open,
-  close,
-  onClick,
-  children,
-  ...props
-}) =>
-  isOpen ? (
-    <Fragment>
-      <LinkButton {...props} onClick={onClick}>
-        Yes
-      </LinkButton>
-      <LinkButton {...props} onClick={close}>
-        No
-      </LinkButton>
-    </Fragment>
-  ) : (
-    <LinkButton {...props} onClick={open}>
-      {children}
-    </LinkButton>
-  );*/
 
 const ConfirmButtonBase = ({
   isOpen,
@@ -51,14 +27,9 @@ const ConfirmButtonBase = ({
   ...props
 }) =>
   isOpen ? (
-    <div className={jstr(props.className, styles.confirm)}>
-      <LinkButton {...props} onClick={onClick}>
-        Yes
-      </LinkButton>
-      <LinkButton {...props} onClick={close}>
-        No
-      </LinkButton>
-    </div>
+    <LinkButton {...props} onClick={onClick} onBlur={close}>
+      Yes?
+    </LinkButton>
   ) : (
     <LinkButton {...props} onClick={open}>
       {children}
