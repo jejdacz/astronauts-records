a8393865c7266441a6d3b8fccbf71976c3038eb0
jejdacz <markhunter@seznam.cz>
2019-01-07 22:53:41 +0100

    hiding nav

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -5,8 +5,9 @@ import withClassName from "../withClassName/withClassName.js";
 import LinkButton from "../LinkButton/LinkButton.js";
 import Container from "../Container/Container.js";
 import styles from "./Nav.module.css";
+import { lifecycle } from "recompose";
 
-export const Nav = ({ children, className, fixed, ...props }) => (
+export const NavBase = ({ children, className, fixed, ...props }) => (
   <Fragment>
     <Container
       fluid={true}
@@ -20,10 +21,47 @@ export const Nav = ({ children, className, fixed, ...props }) => (
   </Fragment>
 );
 
-Nav.propTypes = {
+NavBase.propTypes = {
   children: PropTypes.node,
   fixed: PropTypes.bool,
   className: PropTypes.string
+};
+
+export const Nav = lifecycle({
+  componentDidMount() {
+    window.addEventListener(
+      "scroll",
+      hideOnScrollDown(isScrollingDown(window.scrollY))
+    );
+  },
+  componentWillUnmount() {
+    window.removeEventListener(
+      "scroll",
+      hideOnScrollDown(isScrollingDown(window.scrollY))
+    );
+  }
+})(NavBase);
+
+const isScrollingDown = init => {
+  let prev = init;
+  return () => {
+    const val = window.scrollY > prev;
+    prev = window.scrollY;
+    return val;
+  };
+};
+
+const hideOnScrollDown = isScrollingDown => () => {
+  const classNames = styles.navHiden.split(" ");
+  const element = document.querySelector(`.${styles.nav.split(" ")[0]}`);
+
+  if (element) {
+    if (isScrollingDown()) {
+      classNames.map(c => element.classList.add(c));
+    } else {
+      classNames.map(c => element.classList.remove(c));
+    }
+  }
 };
 
 export const Link = withClassName(styles.link)(LinkButton);
