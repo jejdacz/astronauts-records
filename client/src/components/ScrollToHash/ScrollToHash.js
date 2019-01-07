5e53f487e7e5aaae2ed5620d4739bc04bf92dcbd
jejdacz <markhunter@seznam.cz>
2019-01-07 19:39:26 +0100

    scroll

--- client/src/components/ScrollToHash/ScrollToHash.js
+++ client/src/components/ScrollToHash/ScrollToHash.js
@@ -3,16 +3,20 @@ import { withRouter } from "react-router-dom";
 
 class ScrollToHash extends Component {
   componentDidUpdate(prevProps) {
-    if (this.props.location.key !== prevProps.location.key) {
+    if (this.props.location.pathname !== prevProps.location.pathname) {
+      window.scrollTo(0, 0);
+    } else if (this.props.location.key !== prevProps.location.key) {
       if (this.props.location.hash) {
         document.querySelector(this.props.location.hash) &&
           document
             .querySelector(this.props.location.hash)
             .scrollIntoView({ behavior: "smooth", block: "start" });
       } else {
-        document
-          .querySelector("body")
-          .scrollTo({ top: 0, left: 0, behavior: "smooth" });
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: "smooth"
+        });
       }
     }
   }
